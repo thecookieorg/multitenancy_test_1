1cc596fb3acfe275b96459422f65d78270faa8ab
thecookieorg <marko@thecookie.org>
2016-06-15 14:24:15 -0700

    Added account model, referenced user_id, added post scaffod and referenced account_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615212108) do
+ActiveRecord::Schema.define(version: 20160615212345) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "subdomain"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
